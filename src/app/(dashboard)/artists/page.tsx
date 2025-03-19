b3cc4f53aecb33e881d19480c0942e4b381c65c6
Rajan66 <rajanmaharjan042@gmail.com>
2025-03-19 22:57:50 +0545

    chore(setup): add layout and dashboard routes

---
+++ src/app/(dashboard)/artists/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const page = () => {
+  return <div>artists page</div>;
+};
+
+export default page;
---
+++ src/app/(dashboard)/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const page = () => {
+  return <div>Dashboard overview page</div>;
+};
+
+export default page;
