ee558f6c96b5e8c7626374ccffa7d2dd8c5ad256
Rajan66 <rajanmaharjan042@gmail.com>
2025-03-20 17:43:39 +0545

    fix(login): login schema, set cookie and add toast

--- src/features/auth/schemas/login.schema.ts
+++ src/features/auth/schemas/login.schema.ts
@@ -8,15 +8,7 @@ export const LoginSchema = z.object({
       message: "Invalid email address.",
     })
     .trim(),
-  password: z
-    .string()
-    .min(8, { message: "Must be at least 8 characters long" })
-    .regex(/[a-zA-Z]/, { message: "Must contain at least one letter." })
-    .regex(/[0-9]/, { message: "Must contain at least one number." })
-    .regex(/[^a-zA-Z0-9]/, {
-      message: "Must contain at least one special character.",
-    })
-    .trim(),
+  password: z.string().trim(),
 });
 
 export type TLogin = z.infer<typeof LoginSchema>;
