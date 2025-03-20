c6729fa7d93e4343570242738f15c4afcafd304f
Rajan66 <rajanmaharjan042@gmail.com>
2025-03-20 23:51:18 +0545

    feat(sidebar): implement sidebar

---
+++ src/features/dashboard/components/sidebar.tsx
@@ -0,0 +1,51 @@
+"use client";
+import Image from "next/image";
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarTrigger,
+  SidebarRail,
+  useSidebar,
+} from "@/components/ui/sidebar";
+import SidebarItems from "./sidebar-items";
+
+import logo from "@/app/favicon.ico";
+
+const DashboardSidebar = () => {
+  const { state } = useSidebar();
+  return (
+    <Sidebar
+      collapsible="icon"
+      style={
+        {
+          "--sidebar-width-icon": "4rem",
+        } as React.CSSProperties
+      }
+    >
+      <div className="flex items-center gap-4 p-3">
+        <Image
+          src={logo || "/placeholder.svg"}
+          alt="VoxCloud logo"
+          width={40}
+          height={40}
+        />
+        {state === "expanded" && (
+          <h2 className="text-lg font-semibold">VoxCloud</h2>
+        )}
+      </div>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarItems />
+        </SidebarGroup>
+      </SidebarContent>
+      {state === "expanded" && (
+        <SidebarTrigger className="absolute top-4 right-4" />
+      )}
+      <SidebarRail />
+    </Sidebar>
+  );
+};
+
+export default DashboardSidebar;
