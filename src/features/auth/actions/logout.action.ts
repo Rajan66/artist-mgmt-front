b2eee37522ee44cf0eeaecb95dd8dc215c2c6787
Rajan66 <rajanmaharjan042@gmail.com>
2025-03-21 15:01:33 +0545

    feat(auth): implement logout and blacklist token

--- src/features/auth/actions/logout.action.ts
+++ src/features/auth/actions/logout.action.ts
@@ -1,10 +1,27 @@
 "use server";
-import { redirect } from "next/navigation";
-import { removeCookie } from "@/actions/cookies";
+import { isAxiosError } from "axios";
+
+import { getCookie, removeCookie } from "@/actions/cookies";
+import { PostRequest } from "@/lib/axios/axios";
 
 export const logout = async () => {
-  removeCookie("access_token");
-  removeCookie("refresh_token");
-  removeCookie("user_id");
-  redirect("/login");
+  try {
+    const token = getCookie("refresh_token");
+    removeCookie("access_token");
+    removeCookie("refresh_token");
+    removeCookie("user_id");
+
+    const response = await PostRequest(
+      "/api/v1/auth/token/blacklist/",
+      {},
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    return response.status;
+  } catch (error) {
+    if (isAxiosError(error)) return error?.response?.data;
+  }
 };
