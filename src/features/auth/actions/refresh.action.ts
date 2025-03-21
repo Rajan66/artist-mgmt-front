6a3b589c52e6830956ff9a2803b4b9e41eb9b355
Rajan66 <rajanmaharjan042@gmail.com>
2025-03-21 15:02:20 +0545

    feat(auth): implement refresh token and move middlware

--- src/features/auth/actions/refresh.action.ts
+++ src/features/auth/actions/refresh.action.ts
@@ -1,15 +1,25 @@
-import axios from "axios";
+import axios from "@/lib/axios/interceptor";
+import { isAxiosError } from "axios";
 
 export const getRefreshToken = async (token: string) => {
   try {
-    const response = await axios.post("api/v1/auth/refresh-token/", {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
-    const data = await response.data;
+    const response = await axios.post(
+      "api/v1/auth/token/refresh/",
+      {},
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    const data = await response.data.data;
     return data;
   } catch (error) {
-    console.error(error);
+    if (isAxiosError(error)) {
+      console.error(error.response?.data?.message || "An error occurred");
+      return error.response?.data.message;
+    } else {
+      console.error("Something went wrong", error);
+    }
   }
 };
