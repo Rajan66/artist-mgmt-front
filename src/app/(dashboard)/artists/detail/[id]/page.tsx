e04556cf492b03cd54ccbc0301843b1a9665cd33
Rajan66 <rajanmaharjan042@gmail.com>
2025-03-31 23:40:11 +0545

    feat(artists): add artist details page

---
+++ src/app/(dashboard)/artists/detail/[id]/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { ArtistHero } from "@/features/artists/components";
+
+const page = () => {
+  return (
+    <div>
+      <ArtistHero />
+    </div>
+  );
+};
+
+export default page;
