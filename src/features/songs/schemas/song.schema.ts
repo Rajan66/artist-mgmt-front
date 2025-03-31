67aa9ff3b9278a99b856477d27616d06dda957a2
Rajan66 <rajanmaharjan042@gmail.com>
2025-03-31 12:58:42 +0545

    feat(songs): setup songs crud

---
+++ src/features/songs/schemas/song.schema.ts
@@ -0,0 +1,16 @@
+import { z } from "zod";
+
+export const SongSchema = z.object({
+  title: z
+    .string({ required_error: "Title is required." })
+    .min(1, { message: "Must be at least 1 character long." }),
+  album_id: z
+    .string({ required_error: "Album is required." })
+    .min(1, { message: "Must be at least 1 character long" }),
+  genre: z.string({ required_error: "Genre is required." }),
+  release_date: z.date({
+    required_error: "Release date is required.",
+  }),
+});
+
+export type TSongSchema = z.infer<typeof SongSchema>;
