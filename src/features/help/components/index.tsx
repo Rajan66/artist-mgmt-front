adf7ef29a8319373c2bd0479c40d1fbd38ed11ec
Rajan66 <rajanmaharjan042@gmail.com>
2025-04-06 22:20:43 +0545

    feat(help): add help components

---
+++ src/features/help/components/index.tsx
@@ -0,0 +1,4 @@
+export { default as HelpFooter } from "./footer";
+export { default as HelpHeader } from "./header";
+export { default as HelpFAQ } from "./faq";
+export { default as Help } from "./help";
