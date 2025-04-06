f8ea7281fbba98477cfd661c19f1bca811fe241c
Rajan66 <rajanmaharjan042@gmail.com>
2025-04-06 22:21:08 +0545

    feat(help): add help and settings pages

---
+++ src/app/(dashboard)/settings/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { SettingsPage } from "@/features/settings/components/";
+
+const page = () => {
+  return (
+    <div>
+      <SettingsPage />
+    </div>
+  );
+};
+
+export default page;
