aa7626473d05a3ea804ba30c10837532915b8d67
Rajan66 <rajanmaharjan042@gmail.com>
2025-04-07 17:46:47 +0545

    chore(other): conditional fetching for admin and manager

--- src/features/songs/components/table.tsx
+++ src/features/songs/components/table.tsx
@@ -6,13 +6,16 @@ import DataTable from "@/components/data-table";
 import { columns } from "@/features/songs/components";
 import { getUser } from "@/utils/get-user";
 
-import { useGetManagerSongs } from "../hooks/use-queries";
+import { useGetManagerSongs, useGetSongs } from "../hooks/use-queries";
 import { TSong } from "../types/song.type";
 
 const SongTable = () => {
-  const manager = getUser();
+  const user = getUser();
 
-  const { data } = useGetManagerSongs(manager.id);
+  const { data } =
+    user?.role === "artist_manager"
+      ? useGetManagerSongs(user?.id)
+      : useGetSongs();
 
   return (
     <DataTable<TSong, string[]>
