969e92ebcf9928470e537e7923ca461f34a6a1a8
Rajan66 <rajanmaharjan042@gmail.com>
2025-04-08 14:15:06 +0545

    feat(charts): add data in charts

--- src/components/charts/albums-per-artist-chart.tsx
+++ src/components/charts/albums-per-artist-chart.tsx
@@ -2,42 +2,41 @@
 
 import { Bar, BarChart, CartesianGrid, XAxis, YAxis } from "recharts";
 
+import {
+  useGetAdminArtistAlbumsStats,
+  useGetManagerArtistAlbumsStats,
+} from "@/features/dashboard/hooks/use-queries";
+import { getUser } from "@/utils/get-user";
+
 interface AlbumsPerArtistChartProps {
   className?: string;
 }
 
 const AlbumsPerArtistChart = ({ className }: AlbumsPerArtistChartProps) => {
-  const data = [
-    { artist: "Taylor Swift", albums: 12 },
-    { artist: "Drake", albums: 9 },
-    { artist: "Ed Sheeran", albums: 7 },
-    { artist: "Ariana Grande", albums: 6 },
-    { artist: "The Weeknd", albums: 5 },
-    { artist: "Billie Eilish", albums: 4 },
-    { artist: "Post Malone", albums: 4 },
-    { artist: "Dua Lipa", albums: 3 },
-    { artist: "Justin Bieber", albums: 6 },
-    { artist: "BTS", albums: 8 },
-  ];
+  const user = getUser();
+  const { data } =
+    user?.role === "artist_manager"
+      ? useGetManagerArtistAlbumsStats(user?.id)
+      : useGetAdminArtistAlbumsStats();
 
   return (
     <div className={className}>
       <BarChart
         width={500}
         height={300}
-        data={data}
+        data={data?.data}
         margin={{ top: 5, right: 5, left: 5, bottom: 40 }}
       >
         <CartesianGrid vertical={false} strokeDasharray="3 3" />
         <XAxis
-          dataKey="artist"
+          dataKey="artist_name"
           tickLine={false}
           axisLine={false}
           angle={-45}
           textAnchor="end"
           height={60}
         />
-        <YAxis tickLine={false} axisLine={false} />
+        <YAxis tickLine={false} axisLine={false} allowDecimals={false} />
         <Bar dataKey="albums" fill="var(--chart-2)" radius={[4, 4, 0, 0]} />
       </BarChart>
     </div>
---
+++ src/components/charts/columns.tsx
@@ -0,0 +1,93 @@
+"use client";
+
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+
+import { ColumnDef } from "@tanstack/react-table";
+import { MoreHorizontal } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { api_image } from "@/constants/api";
+import { TSong } from "@/features/songs/types/song.type";
+
+export const columns: ColumnDef<TSong>[] = [
+  {
+    accessorKey: "cover_image",
+    header: "",
+    cell: ({ row }) => (
+      <div>
+        <Image
+          src={
+            row.original.album?.cover_image
+              ? `${api_image}/${row.original.album?.cover_image}`
+              : `/images/album.png`
+          }
+          alt="Cover Image"
+          width={80}
+          height={50}
+          className="rounded-lg w-10 h-10"
+        />
+      </div>
+    ),
+  },
+  {
+    accessorKey: "title",
+    header: "Title",
+  },
+  {
+    accessorKey: "artist",
+    header: "Artist",
+    cell: ({ row }) => row.original.album?.artist.name,
+  },
+  {
+    accessorKey: "genre",
+    header: "Genre",
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      const song = row.original;
+      const router = useRouter();
+
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <MoreHorizontal className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            <DropdownMenuItem
+              onClick={() =>
+                navigator.clipboard?.writeText(song.title.toString())
+              }
+            >
+              Copy song title
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem
+              onClick={() => router.push(`/songs/${song?.album?.artist?.id}`)}
+            >
+              View artist
+            </DropdownMenuItem>
+            <DropdownMenuItem
+              onClick={() => router.push(`/songs/${song?.album?.id}`)}
+            >
+              View album
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+    },
+  },
+];
